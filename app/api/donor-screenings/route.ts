9f1e6643ebd87fd2b1116299a9532228e9e5773c
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-03-31 02:22:36 -0700

    auth mm

--- app/api/donor-screenings/route.ts
+++ app/api/donor-screenings/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth/next"
 import prisma from "@/lib/prisma"
-import { authOptions } from "../../auth/[...nextauth]/route"
+import { authOptions } from "../auth/[...nextauth]/route"
 
 export async function GET(req: NextRequest) {
   try {
